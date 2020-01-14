86caf45d86714e291533df9f13b93596a7b9b41c
ArtemGucci <ArtemGukov2004@yandex.ru>
2020-01-14 13:59:49 +0300

    second commit

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def load_start_fon():
     y = 270
     color = pygame.Color('red')
 
-    fon = pygame.transform.scale(load_image('start_fon.jpg'), (w, h))
+    fon = pygame.transform.scale(load_image('start_fon.jpg'), (width, height))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 50)
     for line in text:
@@ -70,7 +70,7 @@ def load_end_fon():
     y = 600
     color = pygame.Color('purple')
 
-    fon = pygame.transform.scale(load_image('end_fon.jpg'), (w, h))
+    fon = pygame.transform.scale(load_image('end_fon.jpg'), (width, height))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 50)
     for line in text:
@@ -102,7 +102,7 @@ def load_image(name, colorkey=None):
     return image
 
 
-'''def load_level():
+def load_level():
     app = QApplication(argv)
     ex = Level()
     ex.show()
@@ -113,31 +113,102 @@ def load_image(name, colorkey=None):
         lines = [i.strip() for i in f.readlines()]
         h = len(lines)
         w = len(lines[0])
-        return lines, w * 50, h * 50'''
+        return lines
 
 
-#lines, w, h = load_level()
+lines = load_level()
 level_name = ''
-size = 900, 700
+size = width, height = 900, 700
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 hero = pygame.sprite.Group()
 load_start_fon()
 load_end_fon()
-v = None
+objects = pygame.sprite.Group()
+heroes = pygame.sprite.Group()
+speed = 1
+
+
+def make_level():
+    player = None
+    n = width * 25
+    m = height * 25
+    for i in range(n):
+        for j in range(m):
+            if lines[j][i] == '#':
+                Wall((i * 25, j * 25))
+            elif lines[j][i] == '@':
+                Hero((i * 25, j * 25))
+                Grass((i * 25, j * 25))
+            elif lines[j][i] == '.':
+                Grass((i * 25, j * 25))
+            else:
+                Stairs((i * 25, j * 25))
+                Grass((i * 25, j * 25))
 
 
 class Hero(pygame.sprite.Sprite):
-    image = load_image('hero')
+    sheet = load_image('hero.png')
+    columns = rows = 2
 
     def __init__(self, pos):
-        super().__init__(hero)
-        self.image = Hero.image
+        super().__init__(heroes)
+        self.frames = []
+        self.cut_sheet(Hero.sheet, Hero.columns, Hero.rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.image.set_colorkey((255, 255, 255))
         self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = 0, 0
+        self.rect.x, self.rect.y = pos
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
 
     def update(self):
-        pass
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+    def change_direction(self):
+        self.image = pygame.transform.flip(self.image, True, False)
+
+
+class Wall(pygame.sprite.Sprite):
+    image = load_image('wall.jpg')
+
+    def __init__(self, pos):
+        super().__init__(objects)
+        self.image = Wall.image
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = pos
+        self.width = self.height = 25
+
+
+class Grass(pygame.sprite.Sprite):
+    image = load_image('grass.jpg')
+
+    def __init__(self, pos):
+        super().__init__(objects)
+        self.image = Grass.image
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = pos
+        self.width = self.height = 25
+
+
+class Stairs(pygame.sprite.Sprite):
+    image = load_image('stairs.jpg', (255, 255, 255))
+
+    def __init__(self, pos):
+        super().__init__(objects)
+        self.image = Grass.image
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = pos
+        self.width = self.height = 25
 
 
 running = True
